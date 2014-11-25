63f95b4dd7ec4e6df25f0a930669871260a65e16
pre284mish <mishra.p@husky.neu.edu>
2014-11-25 14:21:18 -0800

    commit

--- Table_Query/Restaurant_Reservation_2014-11-25.sql
+++ Table_Query/Restaurant_Reservation_2014-11-25.sql
@@ -7,7 +7,7 @@
 #
 # Host: localhost (MySQL 5.6.21)
 # Database: Restaurant_Reservation
-# Generation Time: 2014-11-25 08:14:50 +0000
+# Generation Time: 2014-11-25 22:19:18 +0000
 # ************************************************************
 
 
@@ -26,14 +26,14 @@
 DROP TABLE IF EXISTS `Address`;
 
 CREATE TABLE `Address` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `street` varchar(225) DEFAULT NULL,
-  `apt_No` int(10) DEFAULT NULL,
-  `city` varchar(20) DEFAULT NULL,
-  `state` varchar(20) DEFAULT NULL,
-  `zip` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `A_id_user_fk` FOREIGN KEY (`id`) REFERENCES `User` (`personId`)
+  `street` varchar(225) NOT NULL DEFAULT '',
+  `apt_No` varchar(10) DEFAULT NULL,
+  `city` varchar(20) NOT NULL DEFAULT '',
+  `state` varchar(20) NOT NULL DEFAULT '',
+  `zip` varchar(20) NOT NULL DEFAULT '',
+  `userName` varchar(225) NOT NULL DEFAULT '',
+  PRIMARY KEY (`userName`),
+  CONSTRAINT `pk_user_fk` FOREIGN KEY (`userName`) REFERENCES `User` (`userName`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
@@ -56,12 +56,12 @@ CREATE TABLE `Category` (
 DROP TABLE IF EXISTS `Favorites`;
 
 CREATE TABLE `Favorites` (
-  `userId` int(11) unsigned NOT NULL,
+  `userName` varchar(225) NOT NULL DEFAULT '',
   `restaurantId` int(11) unsigned NOT NULL,
-  PRIMARY KEY (`userId`),
+  PRIMARY KEY (`userName`),
   KEY `restaurantId_fk` (`restaurantId`),
-  CONSTRAINT `restaurantId_fk` FOREIGN KEY (`restaurantId`) REFERENCES `Restaurant` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `userID_fk` FOREIGN KEY (`userId`) REFERENCES `User` (`personId`) ON DELETE CASCADE
+  CONSTRAINT `pk_user7_fk` FOREIGN KEY (`userName`) REFERENCES `User` (`userName`) ON DELETE CASCADE,
+  CONSTRAINT `restaurantId_fk` FOREIGN KEY (`restaurantId`) REFERENCES `Restaurant` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
@@ -72,12 +72,12 @@ CREATE TABLE `Favorites` (
 DROP TABLE IF EXISTS `Follower`;
 
 CREATE TABLE `Follower` (
-  `Following` int(11) unsigned DEFAULT NULL,
-  `Follower` int(11) unsigned DEFAULT NULL,
-  KEY `user_fk1` (`Following`),
-  KEY `user_fk2` (`Follower`),
-  CONSTRAINT `user_fk1` FOREIGN KEY (`Following`) REFERENCES `User` (`personId`) ON DELETE CASCADE,
-  CONSTRAINT `user_fk2` FOREIGN KEY (`Follower`) REFERENCES `User` (`personId`) ON DELETE CASCADE
+  `Following` varchar(225) DEFAULT '',
+  `Follower` varchar(225) NOT NULL DEFAULT '',
+  KEY `pk_user4_fk` (`Following`),
+  KEY `pk_user5_fk` (`Follower`),
+  CONSTRAINT `pk_user4_fk` FOREIGN KEY (`Following`) REFERENCES `User` (`userName`),
+  CONSTRAINT `pk_user5_fk` FOREIGN KEY (`Follower`) REFERENCES `User` (`userName`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
@@ -88,13 +88,12 @@ CREATE TABLE `Follower` (
 DROP TABLE IF EXISTS `Person`;
 
 CREATE TABLE `Person` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `userName` varchar(100) DEFAULT NULL,
-  `password` varchar(100) DEFAULT NULL,
-  `firstName` varchar(250) DEFAULT NULL,
-  `lastName` varchar(250) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `userName` varchar(225) NOT NULL DEFAULT '',
+  `password` varchar(225) NOT NULL DEFAULT '',
+  `firstName` varchar(250) NOT NULL DEFAULT '',
+  `lastName` varchar(250) NOT NULL DEFAULT '',
+  PRIMARY KEY (`userName`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 
@@ -104,17 +103,17 @@ CREATE TABLE `Person` (
 DROP TABLE IF EXISTS `Reservation`;
 
 CREATE TABLE `Reservation` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(100) unsigned NOT NULL AUTO_INCREMENT,
   `people_count` int(5) DEFAULT NULL,
   `date` datetime DEFAULT NULL,
   `time_slot` datetime DEFAULT NULL,
   `restaurantId` int(11) unsigned NOT NULL,
-  `userId` int(11) unsigned NOT NULL,
+  `userName` varchar(225) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`),
-  KEY `pk_userId_fk` (`userId`),
   KEY `pk_reservation_fk` (`restaurantId`),
+  KEY `pk_user2_fk` (`userName`),
   CONSTRAINT `pk_reservation_fk` FOREIGN KEY (`restaurantId`) REFERENCES `Restaurant` (`id`),
-  CONSTRAINT `pk_userId_fk` FOREIGN KEY (`userId`) REFERENCES `User` (`personId`)
+  CONSTRAINT `pk_user2_fk` FOREIGN KEY (`userName`) REFERENCES `User` (`userName`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
@@ -146,15 +145,15 @@ CREATE TABLE `Restaurant` (
 DROP TABLE IF EXISTS `Reviews`;
 
 CREATE TABLE `Reviews` (
-  `id` int(11) unsigned NOT NULL,
+  `userName` varchar(225) NOT NULL DEFAULT '',
   `ratings` int(2) DEFAULT NULL,
   `comments` varchar(500) DEFAULT NULL,
   `date` datetime DEFAULT NULL,
   `restaurantId` int(11) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`userName`),
   KEY `pk_rest_fk` (`restaurantId`),
   CONSTRAINT `pk_rest_fk` FOREIGN KEY (`restaurantId`) REFERENCES `Restaurant` (`id`),
-  CONSTRAINT `pk_user_fk` FOREIGN KEY (`id`) REFERENCES `User` (`personId`)
+  CONSTRAINT `pk_user1_fk` FOREIGN KEY (`userName`) REFERENCES `User` (`userName`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
@@ -182,10 +181,10 @@ DROP TABLE IF EXISTS `User`;
 
 CREATE TABLE `User` (
   `phoneNo.` int(12) DEFAULT NULL,
-  `email_Id` varchar(225) DEFAULT NULL,
-  `personId` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`personId`),
-  CONSTRAINT `p_id_fk` FOREIGN KEY (`personId`) REFERENCES `Person` (`id`)
+  `email_Id` varchar(225) NOT NULL DEFAULT '',
+  `userName` varchar(225) NOT NULL DEFAULT '',
+  PRIMARY KEY (`userName`),
+  CONSTRAINT `pk_person_fk` FOREIGN KEY (`userName`) REFERENCES `Person` (`userName`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
