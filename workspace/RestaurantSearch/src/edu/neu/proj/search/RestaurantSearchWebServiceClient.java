167cb1d12d5581b0d20caae373ab6cf3825cc658
hsathe <hsathe@ccs.neu.edu>
2014-11-28 19:20:47 -0800

    Search API Issue resolved
    Search API Issue resolved

--- workspace/RestaurantSearch/src/edu/neu/proj/search/RestaurantSearchWebServiceClient.java
+++ workspace/RestaurantSearch/src/edu/neu/proj/search/RestaurantSearchWebServiceClient.java
@@ -1,30 +1,28 @@
 package edu.neu.proj.search;
-
+/**
+ * @author Harshad
+ *
+ */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
-
 import javax.net.ssl.HttpsURLConnection;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class RestaurantSearchWebServiceClient {
 
-	private String urlAPIPlaces = "https://maps.googleapis.com/maps/api/place/textsearch/json?query={NAME} in {LOCATION}&key=AIzaSyCTxX10Hznx4ta5ZvlCS1BFXxDOwNJlQ-s";
-	private String urlAPIPlaceDetails;
-	//private String urlApi = "https://api.worldweatheronline.com/free/v1/weather.ashx?q={{ZIP}},USA&format=json&num_of_days=2&key=s3uv4fjbaw4pqtp26rh48afd";
-
+	private String urlAPIPlaces = "https://maps.googleapis.com/maps/api/place/textsearch/json?query={NAME}+in+{LOCATION}&key=AIzaSyCTxX10Hznx4ta5ZvlCS1BFXxDOwNJlQ-s";
+	private String urlAPIPlaceDetails ="https://maps.googleapis.com/maps/api/place/details/json?placeid={PLACE}&key=AIzaSyCTxX10Hznx4ta5ZvlCS1BFXxDOwNJlQ-s";
 	
-	public void getRestaurantByNameAndLocation(String name, String location) {
-		String urlStr = urlAPIPlaces.replace("{NAME} in {LOCATION}", name
-				+ " in " + location);
-		System.out.println("New URL:" + urlStr);
-
-		
-		//String urlStr1 = urlApi.replace("{{ZIP}}", zip);
-		
-		System.out.println(urlStr);
+	public RestaurantSearch getRestaurantByNameAndLocation(String name, String location) {
+		String urlStr = urlAPIPlaces.replace("{NAME}+in+{LOCATION}", name
+				+ "+in+" + location);
 		try {
 			URL url = new URL(urlStr);
 			HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
@@ -38,21 +36,93 @@ public class RestaurantSearchWebServiceClient {
 			while ((line = buffer.readLine()) != null) {
 				json += line;
 			}
-			
-			System.out.println("JSON: "+json);
+			JSONParser parser = new JSONParser();
+			try {
+				JSONObject root = (JSONObject) parser.parse(json);
+				JSONArray results = (JSONArray) root.get("results");
+				JSONObject firstRestaurant = (JSONObject) results.get(0);
+				String restaurantId = firstRestaurant.get("place_id").toString();
+				RestaurantSearch restaurant = getPlaceDetails(restaurantId);
+				return restaurant;
+				
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
 
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return null;
+	}
+
+	private RestaurantSearch getPlaceDetails(String restaurantId) {
+		String placeDetailURL = urlAPIPlaceDetails.replace("{PLACE}", restaurantId);
+		int day = 0;
+		RestaurantSearch restaurant = new RestaurantSearch();
+		System.out.println("Place detail URL:"+placeDetailURL);
+		
+		try {
+			URL url = new URL(placeDetailURL);
+			HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+			InputStream stream = connection.getInputStream();
+			InputStreamReader reader = new InputStreamReader(stream);
+			BufferedReader buffer = new BufferedReader(reader);
+
+			String json = "";
+			String line = "";
+			int capacity = 50;
+			while ((line = buffer.readLine()) != null) {
+				json += line;
+			}
+			JSONParser parser = new JSONParser();
+			try {
+				JSONObject root = (JSONObject) parser.parse(json);
+				JSONObject restaurantResults = (JSONObject) root.get("result");
+				
+				
+				String address = restaurantResults.get("formatted_address").toString();
+				String phoneNo = restaurantResults.get("formatted_phone_number").toString();
+				String restaurantURL = restaurantResults.get("website").toString();
+				String name = restaurantResults.get("name").toString();
+				
+				JSONObject timings = (JSONObject) restaurantResults.get("opening_hours");
+				JSONArray periods = (JSONArray) timings.get("periods");
+				JSONObject dayTimings = (JSONObject) periods.get(day);
+				JSONObject closeTime = (JSONObject) dayTimings.get("close");
+				JSONObject openTime = (JSONObject) dayTimings.get("open");
+				
+				String closeRestaurantTime = closeTime.get("time").toString();
+				String openRestaurantTime = openTime.get("time").toString();
+
+				restaurant.setName(name);
+				restaurant.setWebsite(restaurantURL);
+				restaurant.setAddress(address);
+				restaurant.setPhoneNo(phoneNo);
+				restaurant.setCapacity(capacity);
+				restaurant.setOpeningTime(openRestaurantTime);
+				restaurant.setClosingTime(closeRestaurantTime);
+
+				return restaurant;
+				
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	public static void main(String[] args) {
+		String name="Chutneys";
+		String location = "Seattle";
 		RestaurantSearchWebServiceClient client = new RestaurantSearchWebServiceClient();
-		client.getRestaurantByNameAndLocation("Chutney", "Seattle");
-
+		RestaurantSearch searchResults = client.getRestaurantByNameAndLocation(name, location);
+		
 	}
 
 }
