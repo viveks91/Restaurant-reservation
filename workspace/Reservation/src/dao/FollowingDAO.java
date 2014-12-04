f4f62ef25b390c199c77071cdfe7f79a885da50d
Vivek <s.vivekananda007@gmail.com>
2014-12-04 15:33:26 -0800

    Created dynamic web project

---
+++ workspace/Reservation/src/dao/FollowingDAO.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import models.Following;
+import models.Reviews;
+
+
+/**
+ * @author preetymishra
+ *
+ */
+public class FollowingDAO {
+
+	EntityManagerFactory factory = Persistence.createEntityManagerFactory("Restaurant_Reservation");
+	EntityManager em = null;
+	
+	public FollowingDAO()	{
+		em = factory.createEntityManager();
+	}
+	public Following create(Following following) {
+		em.getTransaction().begin();
+		em.persist(following);
+		em.getTransaction().commit();
+		return following;
+	}
+	
+	public void deleteByUserNameAndFollowing(String userName, String following) {
+		em.getTransaction().begin();
+		Query q = em.createNamedQuery("Following.deleteByUserNameAndFollowing");
+		q.setParameter("userName", userName);
+		q.setParameter("following", following);
+		q.executeUpdate();
+		em.getTransaction().commit();
+	}
+	
+	public List<Following> findByUserName(String userName) {
+		em.getTransaction().begin();
+		Query q = em.createNamedQuery("Following.findByUserName");
+		q.setParameter("userName", userName);
+		List<Following> following = q.getResultList();
+		em.getTransaction().commit();
+		return following;
+	}
+	
+	
+	public static void main(String[] args) {
+		FollowingDAO dao = new FollowingDAO();
+		/*Following f1 = new Following("pre284", "pre2718");
+		dao.create(f1);
+		Following f2 = new Following("pre284", "pre2712");
+		dao.create(f2);
+		//dao.deleteByUserNameAndFollowing("pre284", "pre2718");*/
+		List<Following> f3 = dao.findByUserName("pre284");
+		System.out.println(f3);
+
+	}
+
+}
---
+++ workspace/Reservation/src/models/Address.java
@@ -0,0 +1,98 @@
+package models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+
+/**
+ * Entity implementation class for Entity: Address
+ * @author Vivek
+ *
+ */
+@Entity
+@NamedQuery(
+		name="Address.findByAll", 
+		query="select a from Address a where a.street = :street and "
+		+ "a.apt_no = :apt_no and a.city = :city and a.state =:state and a.zip =:zip")
+public class Address implements Serializable {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String street;
+	private String apt_no;
+	private String city;
+	private String state;
+	private String zip;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getApt_no() {
+		return apt_no;
+	}
+	public void setApt_no(String apt_no) {
+		this.apt_no = apt_no;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	public Address(Integer id, String street, String apt_no, String city,
+			String state, String zip) {
+		super();
+		this.id = id;
+		this.street = street;
+		this.apt_no = apt_no;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+	}
+	public Address(String street, String apt_no, String city, String state,
+			String zip) {
+		super();
+		this.street = street;
+		this.apt_no = apt_no;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+	}
+	public Address() {
+		super();
+	}
+	public Address(String street, String city, String state, String zip) {
+		super();
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.apt_no = null;
+	}
+	
+
+}
